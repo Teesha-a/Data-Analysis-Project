a74295f5d37ead2e1e62bc07bd4d84ea7de1f3dc
teesha <131791170+Teesha-a@users.noreply.github.com>
2025-03-07 20:22:44 +0530

    Update Edtech_Website_Exploratory_Data_Analysis(Udemy).py

--- Edtech_Website_Exploratory_Data_Analysis(Udemy).py
+++ Edtech_Website_Exploratory_Data_Analysis(Udemy).py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"C:\Users\teesh\Downloads\udemy_courses.csv")
+df = pd.read_csv(r".\udemy_courses.csv")
 
 
 # **************DISPLAYING DATASET HAVING INFO ABOUT COURSES PROVIDED BY UDEMY*******************
